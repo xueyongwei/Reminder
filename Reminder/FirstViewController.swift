331dba1b4c3f6c2d90339701d068e5cba9865673
薛永伟 <xueyongwei@foxmail.com>
2018-09-30 11:43:15 +0800

    Initial Commit

---
+++ Reminder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Reminder
+//
+//  Created by 薛永伟 on 2018/9/30.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Reminder/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Reminder
+//
+//  Created by 薛永伟 on 2018/9/30.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
